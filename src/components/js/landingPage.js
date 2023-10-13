d044ff82b4735dce06c0932b6ea14c227a493bc1
kotanagasruthi <nagasruthikota@gmail.com>
2023-10-13 22:07:15 +0530

    landing page code

---
+++ src/components/js/landingPage.js
@@ -0,0 +1,12 @@
+export default {
+  methods: {
+    login () {
+      // Handle the login logic here
+      console.log('Login button clicked')
+    },
+    signup () {
+      // Handle the signup logic here
+      console.log('Signup button clicked')
+    }
+  }
+}
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="dashboard">
+    <div class="half image-side">
+      <img src="path_to_your_image.jpg" alt="Image Description">
+    </div>
+    <div class="half content-side">
+      <img src="path_to_vidhyasoft_logo.png" alt="VidhyaSoft Logo" class="company-logo">
+      <h1>VidhyaSoft</h1>
+      <button @click="login">Login</button>
+      <button @click="signup">Signup</button>
+    </div>
+  </div>
+</template>
+
+<script src='../components/js/landingPage.js'></script>
+
+<style scoped>
+      @import '../styles/landing-page.scss';
+</style>
