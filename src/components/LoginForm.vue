be647941765b4275fc2b6260c51a6e6284034800
kotanagasruthi <nagasruthikota@gmail.com>
2023-10-14 12:09:47 +0530

    login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,43 +1,67 @@
 <template>
-  <div class="custom-input-box">
-    <fieldset>
-      <legend>Email</legend>
-      <input type="email">
+  <div class="login-form">
+
+    <fieldset class="login-form-fieldset">
+      <legend class="login-form-fieldset-legend">Institute ID*</legend>
+      <input type="text" class="login-form-fieldset-input"  v-model="instituteID">
     </fieldset>
+
+    <fieldset class="login-form-fieldset">
+      <legend class="login-form-fieldset-legend">Password*</legend>
+      <input type="text" class="login-form-fieldset-input"  v-model="institutePassword">
+    </fieldset>
+
+    <div class="login-form-button">Login</div>
   </div>
 </template>
 
 <script>
 export default {
-  // Your component logic here
+  data () {
+    return {
+      instituteID: '',
+      institutePassword: ''
+    }
+  }
 }
 </script>
 
-<style scoped>
-.custom-input-box {
-  width: 300px;
-}
+<style lang="scss" scoped>
+  .login-form {
+    width: 350px;
+    margin: auto;
+    &-fieldset {
+      border: 1px solid #e0e0e0;
+      padding: 5px 10px;
+      border-radius: 5px;
+      position: relative;
+      margin-top: 15px;
+      &-legend {
+        background-color: #fff;
+        padding: 0 5px;
+        position: absolute;
+        top: -10px;
+        left: 10px;
+      }
 
-fieldset {
-  border: 1px solid #e0e0e0;
-  padding: 5px 10px;
-  border-radius: 5px;
-  position: relative;
-}
+      &-input {
+        border: none;
+        width: 100%;
+        outline: none;
+        padding: 8px 0;
+      }
+    }
+    &-button {
+      background-color: #000;
+      color: #fff;
+      padding: 5px 10px;
+      cursor: pointer;
+      font-size: 16px;
+      font-weight: 600;
+      margin: 15px auto;
+      border-radius: 4px;
+      width: 150px;
+    }
+  }
 
-legend {
-  /* font-weight: bold; */
-  background-color: #fff; /* This will ensure the legend has the same background as its parent container */
-  padding: 0 5px; /* Adds a little spacing around the legend text */
-  position: absolute; /* Takes the legend out of the flow */
-  top: -10px; /* Moves it up to be centered over the top border */
-  left: 10px; /* Small left spacing */
-}
-
-input[type="email"] {
-  border: none; /* Remove default input borders */
-  width: 100%; /* Make input take full width */
-  outline: none; /* Remove focus outline for aesthetics */
-  padding: 5px 0; /* Add a little vertical padding */
-}
 </style>
